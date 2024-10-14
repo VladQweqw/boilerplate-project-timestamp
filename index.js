0467772af7d97f1c147535ec987e64a97f0ef717
Poienariu Vlad <vladpoienariu@gmail.com>
2024-10-14 21:05:52 +0000

    another appraoch

--- index.js
+++ index.js
@@ -25,31 +25,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
-app.get("/api/:date", function(req, res) {
-  let date;
-  if(!isNaN(req.params.date)) {
-    date = new Date(+req.params.date);
-  }else {
-    date = new Date(req.params.date);
+app.get("/api/:date?", function(req, res) {
+  let date = new Date(req.params.date)
+
+  if(isInvalidDate(date)) {
+    date = new Date(+req.params.date)
   }
 
-  if(date.toUTCString() === "Invalid Date") {
-    return res.json({
-      error: "Invalid Date"
-    })
-  }else {
-
-    return res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString(),
-    })
+  if(isInvalidDate(date)) {
+    res.json({error: "Invalid Date"})
+    return
   }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
 })
 
 app.get("/api", function(req, res) {
   const date = new Date();
-  
+
   return res.json({
     unix: date.getTime(),
     utc: date.toUTCString(),
